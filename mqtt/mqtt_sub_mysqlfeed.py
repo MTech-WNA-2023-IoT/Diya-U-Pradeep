8a1150b2916b7703eb20ea108c55544e6785fa3a
Diya U Pradeep <101919764+diyaup@users.noreply.github.com>
2023-04-16 23:14:25 -0700

    Update mqtt_sub_mysqlfeed.py

--- mqtt/mqtt_sub_mysqlfeed.py
+++ mqtt/mqtt_sub_mysqlfeed.py
@@ -23,7 +23,19 @@ def _on_message(client, userdata, msg):
 # 	print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
 	print(msg.topic+" "+str(msg.payload))
   
-
+conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, name, age and place
+data={'topic':'IOT/test','data':(msg.payload)}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO `iot_data`(`topic`,`data`)VALUES(%(topic)s,%(data)s);",data)
+#Close the cursor
+#cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
 	 
 #Subscribed Topics 
 def _on_connect(mqttclient, userdata, flags, rc):
@@ -39,16 +51,4 @@ mqttclient.on_connect = _on_connect
 
 mqttclient.loop_forever()
 
-conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
-#Create a MySQL Cursor to that executes the SQLs
-cur=conn.cursor()
-#Create a dictonary containing the fields, name, age and place
-data={'topic':'IOT/test','data':(msg.payload)}
-#Execute the SQL to write data to the database
-cur.execute("INSERT INTO `iot_data`(`topic`,`data`)VALUES(%(topic)s,%(data)s);",data)
-#Close the cursor
-#cur.close()
-#Commit the data to the database
-conn.commit()
-#Close the connection to the database
-conn.close()
+
