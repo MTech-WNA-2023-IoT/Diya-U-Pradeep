9df014f483d7c72c1f129422740c8e8d7cf09640
Diya U Pradeep <101919764+diyaup@users.noreply.github.com>
2023-04-16 22:39:02 -0700

    Update mqtt_sub_mysqlfeed.py

--- mqtt/mqtt_sub_mysqlfeed.py
+++ mqtt/mqtt_sub_mysqlfeed.py
@@ -27,7 +27,7 @@ conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupra
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
-data={'topic':'IOT/test','data':'test'}
+data={'topic':'IOT/test','data':45.0}
 #Execute the SQL to write data to the database
 cur.execute("INSERT INTO `iot_data`(`topic`,`data`)VALUES(%(topic)s,%(data)s);",data)
 #Close the cursor
