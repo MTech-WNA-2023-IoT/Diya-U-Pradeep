668feab86f69d2f9647ee1210724ff2a6ef858ab
Diya U Pradeep <101919764+diyaup@users.noreply.github.com>
2023-06-20 01:06:45 -0700

    Update fetch_weather_restAPI_mysql.py

--- API/fetch_weather_restAPI_mysql.py
+++ API/fetch_weather_restAPI_mysql.py
@@ -1,5 +1,8 @@
 import json
 from urllib.request import urlopen
+import pymysql
+import mysql.connecor
+
 #Create user account and obtain API key from https://www.weatherapi.com
 
 url = "https://api.weatherapi.com/v1/current.json?key=4e06a340688349e18e3151111231506&q=kollam&aqi=no"
@@ -15,21 +18,43 @@ print(json_api)
 print("Parsed")
 data= json_api['location']
 print(data)
+# Connect to MySQL database
+db = mysql.connector.connect(
+    host='0.0.0.0',
+    user='pi',
+    password='raspberry',
+    database='API'
+)
+# Execute the create table query
+db_cursor = db.cursor()
+# db_cursor.execute(create_table_query)
 
 #Import pymysql module library
 import pymysql
+import mysql.connecor
 #Create a connection to MySQL Database 
-conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
-#Create a MySQL Cursor to that executes the SQLs
-cur=conn.cursor()
-#Create a dictonary containing the fields, name, age and place
-#data={'topic':'temp','data':30}
-#Execute the SQL to write data to the database
-cur.execute("INSERT INTO `iot_data`(`topic`,`data`)VALUES(%(topic)s,%(data)s);",data)
-#Close the cursor
-#cur.close()
-#Commit the data to the database
-conn.commit()
-#Close the connection to the database
-conn.close()
+#conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
+sql = """
+INSERT INTO `weather_data` (`location_name`, `region`, `country`, `latitude`, `longitude`, `timezone_id`, `localtime_epoch`, `localtime`)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+"""
+# Prepare the values for the SQL statement
+values = (location_data["name"],location_data["region"],location_data["country"],location_data["lat"],location_data["lon"],location_data["tz_id"],location_data["localtime_epoch"],location_data["localtime"])
+
+
+# Execute the SQL statement
+db_cursor.execute(sql, values)
+# Commit the changes
+
+db.commit()
+
+# Close the connection
+
+db.close()
+
+# print("Raw Data")
+# print(json_api)
+
+print("Data saved to MySQL successfully!")          
+          
 
