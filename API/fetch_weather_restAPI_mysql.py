f31f435fdf84cb4ab19537913efe987de018fbed
Diya U Pradeep <101919764+diyaup@users.noreply.github.com>
2023-06-20 01:20:08 -0700

    Update fetch_weather_restAPI_mysql.py

--- API/fetch_weather_restAPI_mysql.py
+++ API/fetch_weather_restAPI_mysql.py
@@ -31,7 +31,7 @@ print(data)
 
 #Import pymysql module library
 import pymysql
-import mysql.connector
+#import mysql.connector
 #Create a connection to MySQL Database 
 conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
 sql = """
