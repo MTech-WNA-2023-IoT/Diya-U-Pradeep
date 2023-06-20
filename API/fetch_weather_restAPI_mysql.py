c44669263e3cb55cc7d07336fff2a67afcc84093
Diya U Pradeep <101919764+diyaup@users.noreply.github.com>
2023-06-19 23:36:03 -0700

    Update fetch_weather_restAPI_mysql.py

--- API/fetch_weather_restAPI_mysql.py
+++ API/fetch_weather_restAPI_mysql.py
@@ -16,50 +16,20 @@ print("Parsed")
 data= json_api['location']
 print(data)
 
-from flask import Flask, jsonify,request
-from flaskext.mysql import MySQL
+#Import pymysql module library
+import pymysql
+#Create a connection to MySQL Database 
+conn =pymysql.connect(database="iot_diya",user="diyaupradeep",password="diyaupradeep",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, name, age and place
+#data={'topic':'temp','data':30}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO `iot_data`(`topic`,`data`)VALUES(%(topic)s,%(data)s);",data)
+#Close the cursor
+#cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
 
-#assign a Flask Class
-app=Flask(__name__)
-
-#assign Flask-Mysql Module
-mysql = MySQL()
-
-#Configure MySQL
-app.config['MYSQL_DATABASE_USER'] = 'diyaupradeep'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'diyaupradeep'
-app.config['MYSQL_DATABASE_DB'] = 'iot_diya'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-#initialise MySQL (connect to mysql)
-mysql.init_app(app)
-
-#Create 1st API function for testing
-#once you open "localhost:<port>/" this function is executed.
-#this is not mandatory but will help in testing the program
-
-@app.route('/')
-#Give a funtion name
-def welcome():
-#return is the data given by the API
-	return "\tWelcome.... \tKindly use one of the APIs to get data"
-
-#Create your first real API- "recentlocation" is the API that gives recent device location
-#This API will fetch data from the MySQL Database and display it on the URL
-
-@app.route('/API1')
-def recentlocation():
-#Create a MySQL Cursor	
-	cur = mysql.connect().cursor()
-#Execute the SQL
-	cur.execute('select * from <weather_data> ORDER BY id DESC LIMIT 5 ')
-#Receive the SQL Response in a variable
-	r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-#Return the respose to the URL
-	return jsonify({'Data' : r})
-
-#Start the Flask program
-if __name__ == '__main__':
-#app.run will make the APIs available on this particular IP address and Port 5000
-#0.0.0.0  ip means any one can access.
-    app.run(host="0.0.0.0",debug=1)
